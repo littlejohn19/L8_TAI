9b04f80204f3de8b5c91b0e00762244c1f516ee5
Darek <darekpiwko@outlook.com>
2021-04-26 07:55:51 +0200

    change

--- app/app.js
+++ app/app.js
@@ -16,22 +16,8 @@ app.use(bodyParser.json({limit: '2048kb'}));
 
 app.use(cors());
 
-app.use(express.static('frontend/app'));
+app.use(express.static('public'));
 
-mongoose.connect(config.databaseUrl, {useNewUrlParser: true, useCreateIndex: true}, (error) => {
-    if (error) {
-        console.error(error);
-    }
-    else {
-        console.log('Connect with database established');
-    }
-});
-process.on('SIGINT', () => {
-    mongoose.connection.close(function () {
-        console.error('Mongoose default connection disconnected through app termination');
-        process.exit(0);
-    });
-});
 
 routes(app);
 app.listen(config.port, () => {
